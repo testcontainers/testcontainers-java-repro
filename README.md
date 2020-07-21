12ec8d7c8a353f4c80f68d3491b2d487f78f4ee9
Richard North <rich.north@gmail.com>
2020-07-21 08:35:15 +0100

    Initial repro example template

---
+++ README.md
@@ -0,0 +1,7 @@
+# testcontainers-java-repro
+
+A template repository intended to help create isolated repro examples for [testcontainers-java](https://github.com/testcontainers/testcontainers-java) issues.
+
+To use this as a template, click the `Use this template` button above.
+
+Code to reproduce the issue should be located in [ReproExample](src/test/java/org/testcontainers/repro/ReproExample.java).
---
+++ src/test/java/org/testcontainers/repro/ReproExample.java
@@ -0,0 +1,32 @@
+package org.testcontainers.repro;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class ReproExample {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReproExample.class);
+
+    /**
+     * Placeholder for a piece of code that demonstrates the bug. You can use this as a starting point, or replace
+     * entirely.
+     * <p>
+     * Ideally this would be a failing test. If it's excessively difficult to form as a test (e.g. relates to log
+     * output, teardown or other side effects) then it would be sufficient to explain the behaviour in the issue
+     * description.
+     */
+    @Test
+    public void demonstration() {
+        try (
+            // customize the creation of a container as required
+            GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("redis:6.0.5"))
+        ) {
+            container.start();
+
+            // ...
+        }
+    }
+}
